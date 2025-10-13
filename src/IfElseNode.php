992ece43e539e5b15dbbf8bc92192426b07e9b65
KhanhIceTea <khanhicetea@gmail.com>
2025-10-13 22:24:06 +0700

    PHPDoc

--- src/IfElseNode.php
+++ src/IfElseNode.php
@@ -6,14 +6,42 @@ use Exception;
 
 use function count;
 
+/**
+ * Represents a conditional node that supports if-elseif-else logic in the DOM tree.
+ *
+ * This node allows for conditional rendering of content based on multiple conditions.
+ * It behaves similarly to PHP's if-elseif-else statements but in a DOM context.
+ */
 class IfElseNode extends Node
 {
+    /**
+     * Array of conditions to evaluate for each if/elseif block.
+     *
+     * @var array
+     */
     protected $conditions = [];
 
+    /**
+     * Array of child nodes to render when no conditions are met (else block).
+     *
+     * @var array
+     */
     protected $elseChildren = [];
 
+    /**
+     * Current condition index, tracks which condition block is being built.
+     *
+     * @var int
+     */
     protected $conditionIdx = -1;
 
+    /**
+     * Constructs a new IfElseNode.
+     *
+     * @param  array  $children  Initial children for the first condition block
+     * @param  array  $elseChildren  Children for the else block
+     * @param  mixed  $condition  The initial condition to evaluate
+     */
     public function __construct(
         array $children = [],
         array $elseChildren = [],
@@ -24,12 +52,26 @@ class IfElseNode extends Node
         $this->pushCondition($condition);
     }
 
+    /**
+     * Adds a new condition to the conditions array and increments the condition index.
+     *
+     * @param  mixed  $condition  The condition to add (can be a boolean, callable, or any evaluable value)
+     * @return void
+     */
     public function pushCondition($condition)
     {
         $this->conditions[] = $condition;
         $this->conditionIdx++;
     }
 
+    /**
+     * Adds children to the else block.
+     *
+     * These children will be rendered when no conditions evaluate to true.
+     *
+     * @param  mixed  ...$children  The child nodes to add to the else block
+     * @return static Returns the current instance for method chaining
+     */
     public function else(...$children): static
     {
         foreach ($children as $child) {
@@ -43,6 +85,14 @@ class IfElseNode extends Node
         return $this;
     }
 
+    /**
+     * Adds an elseif condition to the node.
+     *
+     * This creates a new condition block that will be evaluated if previous conditions fail.
+     *
+     * @param  mixed  $condition  The condition to evaluate for this elseif block
+     * @return static Returns the current instance for method chaining
+     */
     public function elseif($condition): static
     {
         $this->pushCondition($condition);
@@ -50,6 +100,17 @@ class IfElseNode extends Node
         return $this;
     }
 
+    /**
+     * Adds children to the current condition block.
+     *
+     * This magic method allows adding children using function call syntax.
+     * Children are added to the current condition block based on the condition index.
+     *
+     * @param  mixed  ...$children  The child nodes to add to the current condition block
+     * @return static Returns the current instance for method chaining
+     *
+     * @throws Exception If children are added after else children have been set
+     */
     public function __invoke(...$children): static
     {
         if (count($this->elseChildren) > 0) {
@@ -72,27 +133,23 @@ class IfElseNode extends Node
         return $this;
     }
 
+    /**
+     * Converts the node to its string representation.
+     *
+     * Evaluates all conditions in order and returns the string representation
+     * of the first condition block that evaluates to true. If no conditions
+     * evaluate to true, returns the else block content.
+     *
+     * @return string The rendered content of the matching condition block or else block
+     */
     public function __toString()
     {
         for ($i = 0; $i <= $this->conditionIdx; $i++) {
             if ($this->tryEvalClosure($this->conditions[$i])) {
-                return (string) (new SlotNode($this->children[$i]));
+                return (string) (new SlotNode([$this->children[$i]]));
             }
         }
 
         return (string) (new SlotNode($this->elseChildren));
     }
-
-    // TODO : String buffering
-    // public function echo()
-    // {
-    //     for ($i = 0; $i <= $this->conditionIdx; $i++) {
-    //         if ($this->tryEvalClosure($this->conditions[$i])) {
-    //             (new SlotNode($this->children[$i]))->echo();
-    //             return;
-    //         }
-    //     }
-    //
-    //     (new SlotNode($this->elseChildren))->echo();
-    // }
 }
--- src/SlotNode.php
+++ src/SlotNode.php
@@ -6,10 +6,30 @@ use Closure;
 
 use function call_user_func;
 
+/**
+ * SlotNode represents a dynamic node that can render content either through a callable function
+ * or its child elements.
+ *
+ * This class extends Node and provides flexibility in rendering content by allowing either:
+ * - A closure function that generates the content dynamically
+ * - Static child elements that are rendered as-is
+ */
 class SlotNode extends Node
 {
+    /**
+     * The closure function that will be called to generate the node's content.
+     * If null, the node will render its children instead.
+     *
+     * @var Closure|null
+     */
     protected $slotFunction;
 
+    /**
+     * Create a new SlotNode instance.
+     *
+     * @param  array  $children  Array of child nodes to be rendered when no slot function is provided
+     * @param  Closure|null  $slotFunction  Optional closure that generates the node's content when called
+     */
     public function __construct(
         array $children = [],
         ?Closure $slotFunction = null,
@@ -18,6 +38,14 @@ class SlotNode extends Node
         $this->slotFunction = $slotFunction;
     }
 
+    /**
+     * Convert the SlotNode to its string representation.
+     *
+     * If a slot function is provided, it will be called and its return value will be cast to string.
+     * Otherwise, the node's children will be rendered as a string.
+     *
+     * @return string The rendered content of the node
+     */
     public function __toString()
     {
         if ($this->slotFunction instanceof Closure) {
@@ -26,21 +54,4 @@ class SlotNode extends Node
 
         return $this->childrenToString();
     }
-
-    // TODO : String buffering
-    // public function echo()
-    // {
-    //     if ($this->slotFunction instanceof Closure) {
-    //         $resolved = $this->tryEvalClosure($this->slotFunction);
-    //
-    //         if ($resolved instanceof Node) {
-    //             $resolved->echo();
-    //             return;
-    //         }
-    //
-    //         echo $resolved;
-    //     }
-    //
-    //     $this->echoChildren();
-    // }
 }
